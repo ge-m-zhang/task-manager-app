0339feeeb69381cc21e5c3fab288bf9237d44930
gezhang <ge.melody.zhang@gmail.com>
2025-07-15 14:18:47 -0400

    added and updated files for unit test config

---
+++ jest.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  },
+}; 
